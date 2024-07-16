13783ac6fa5ebe61b1d5c001640d60d71747aaab
igorm <igorm@IGOR-PC>
2024-07-16 00:14:47 -0300

    Adicionar arquivos de projeto.

---
+++ AlphaDownloadManager/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace AlphaDownloadManager
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.Label lblDownloadingFile;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.lblDownloadingFile = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(650, 292);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(100, 43);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.BackColor = System.Drawing.Color.White;
+            this.progressBar.Location = new System.Drawing.Point(12, 292);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(632, 43);
+            this.progressBar.TabIndex = 1;
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.BackColor = System.Drawing.Color.Transparent;
+            this.lblSpeed.ForeColor = System.Drawing.Color.White;
+            this.lblSpeed.Location = new System.Drawing.Point(516, 276);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(128, 13);
+            this.lblSpeed.TabIndex = 2;
+            this.lblSpeed.Text = "Download Speed: 0 KB/s";
+            this.lblSpeed.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblDownloadingFile
+            // 
+            this.lblDownloadingFile.AutoSize = true;
+            this.lblDownloadingFile.BackColor = System.Drawing.Color.Transparent;
+            this.lblDownloadingFile.ForeColor = System.Drawing.Color.White;
+            this.lblDownloadingFile.Location = new System.Drawing.Point(12, 276);
+            this.lblDownloadingFile.Name = "lblDownloadingFile";
+            this.lblDownloadingFile.Size = new System.Drawing.Size(120, 13);
+            this.lblDownloadingFile.TabIndex = 3;
+            this.lblDownloadingFile.Text = "Downloading File: None";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(332, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Welcome to Alpha Gaming Download Manager!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(368, 96);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Please click the Download button, select the folder where you\r\nwant to download t" +
+    "he AlphaGaming Installer, and wait for the \r\ndownload to complete.\r\n\r\nBest regar" +
+    "ds,\r\nAlphaGaming Team.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(238, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "AlphaGaming Download Manager";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Image = global::AlphaDownloadManager.Properties.Resources.close_256x256;
+            this.pictureBox2.Location = new System.Drawing.Point(734, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.BackgroundImage = global::AlphaDownloadManager.Properties.Resources.Sem_Titulo_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(762, 347);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDownloadingFile);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnDownload);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alpha Download Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ AlphaDownloadManager/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlphaDownloadManager
+{
+    public partial class Form1 : Form
+    {
+        private HttpClient _httpClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _httpClient = new HttpClient();
+        }
+
+        private async Task DownloadFileAsync(string fileUrl, string filePath)
+        {
+            try
+            {
+                using (var response = await _httpClient.GetAsync(fileUrl, HttpCompletionOption.ResponseHeadersRead))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    using (var contentStream = await response.Content.ReadAsStreamAsync())
+                    using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None, 8192, true))
+                    {
+                        var buffer = new byte[8192];
+                        int bytesRead;
+                        long totalBytes = response.Content.Headers.ContentLength ?? -1;
+                        long downloadedBytes = 0;
+
+                        var stopwatch = Stopwatch.StartNew();
+
+                        while ((bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                        {
+                            await fileStream.WriteAsync(buffer, 0, bytesRead);
+                            downloadedBytes += bytesRead;
+
+                            double downloadSpeed = downloadedBytes / stopwatch.Elapsed.TotalSeconds / 1024;
+                            Invoke(new Action(() =>
+                            {
+                                lblSpeed.Text = $"Download Speed: {downloadSpeed:F2} KB/s";
+                            }));
+
+                            // Atualizar progressBar de forma segura
+                        }
+
+                        stopwatch.Stop();
+
+                        // Verificação de integridade do arquivo
+                        if (totalBytes != -1 && downloadedBytes != totalBytes)
+                        {
+                            throw new Exception("O tamanho do arquivo baixado não corresponde ao tamanho esperado.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Re-throw the exception to be caught in the btnDownload_Click method
+                throw new Exception($"Erro ao baixar o arquivo de {fileUrl}: {ex.Message}", ex);
+            }
+        }
+
+
+
+
+        private async void btnDownload_Click(object sender, EventArgs e)
+        {   
+            btnDownload.Enabled = false;
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string downloadFolderPath = folderDialog.SelectedPath;
+                    string txtFileUrl = "https://cdn.cfalphagaming.com/files.txt"; // URL do arquivo .txt com os links
+
+                    try
+                    {
+                        List<string> fileUrls = await ReadUrlsFromTextFile(txtFileUrl);
+
+                        if (fileUrls.Count == 0)
+                        {
+                            MessageBox.Show("Nenhum link encontrado no arquivo .txt.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+
+                        progressBar.Value = 0;
+                        progressBar.Maximum = fileUrls.Count;
+
+                        foreach (var fileUrl in fileUrls)
+                        {
+                            string fileName = Path.GetFileName(new Uri(fileUrl).LocalPath);
+                            lblDownloadingFile.Text = $"Downloading File: {fileName}";
+                            string filePath = Path.Combine(downloadFolderPath, fileName);
+
+                            try
+                            {
+                                await DownloadFileAsync(fileUrl, filePath);
+
+                                // Atualizar a barra de progresso após cada arquivo baixado
+                                progressBar.Value++;
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show($"Erro ao baixar o arquivo: {fileName}\n\n{ex.Message}", "Erro de Download", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                // Continuar o loop para tentar baixar os próximos arquivos
+                            }
+                        }
+
+                        MessageBox.Show("Download concluído!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        btnDownload.Enabled = true;
+                        lblDownloadingFile.Text = "Downloading File: None";
+                        lblSpeed.Text = "Download Speed: 0 KB/s";
+
+                        // Execute the setup file
+                        string setupFilePath = Path.Combine(downloadFolderPath, "AlphaGaming_Setup.exe");
+                        if (File.Exists(setupFilePath))
+                        {
+                            try
+                            {
+                                Process.Start(setupFilePath);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show($"Erro ao executar o arquivo de setup: {ex.Message}", "Erro de Execução", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Arquivo de setup não encontrado.", "Erro de Execução", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Erro ao ler o arquivo .txt: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    btnDownload.Enabled = true;
+                }
+            }
+        }
+
+        private async Task<List<string>> ReadUrlsFromTextFile(string txtFileUrl)
+        {
+            List<string> urls = new List<string>();
+
+            try
+            {
+                using (var response = await _httpClient.GetAsync(txtFileUrl))
+                {
+                    response.EnsureSuccessStatusCode();
+                    string content = await response.Content.ReadAsStringAsync();
+                    string[] lines = content.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (var line in lines)
+                    {
+                        urls.Add(line.Trim());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao ler o arquivo .txt de {txtFileUrl}: {ex.Message}", ex);
+            }
+
+            return urls;
+        }
+
+
+
+
+
+        // Enable window dragging from background
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImport("User32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("User32.dll")]
+        public static extern bool SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
